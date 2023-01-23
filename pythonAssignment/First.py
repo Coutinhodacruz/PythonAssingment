9e489ab211a5cf72ad30bf5b54265a9c6f27613e
Onukwufor dominic <dominicrotimi@gmail.com>
2023-01-22 19:25:33 -0500

    Add files via upload

---
+++ pythonAssignment/First.py
@@ -0,0 +1,236 @@
+score = 0
+  counter = 3
+    while counter<3:
+        answer = input("Make your choice >>>>  ")
+        if answer == "c":
+            print("Correct!")
+            score += 1
+        else:
+            print("That is incorrect. Try again.")
+            counter = counter +1
+  print("Wellcome to quiz game !!")
+  print('if your spelling is incorrect then you are wrong answer')
+score = 0
+
+question_no = 0
+playing = input('Do you want to play ? ').lower()
+
+if playing == 'yes':
+
+    question_no = 1
+    ques = input(f'\n (question_no) .The Capital of nigeria is? ').lower()
+    if ques == 'Abuja':
+        score +=1
+        print('correct!')
+     
+        
+    else:
+        print('Incorrect!')
+        print(f'current answer is --> Abuja')
+
+    question_no += 1
+    ques = input(f'\n(question_no).Who is the Governor of lagos state? ').lower()
+    
+    if ques == 'Sanwolu':
+        print('correct!')
+        
+    else:														
+        print('Incorrect!')
+        print(f'current answer is --> Sanwolu')
+
+
+    question_no += 1
+    ques = input(f'\n(question_no). Spell Government? ').lower()
+    
+    if ques == 'Government':
+        print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Gorvernment')
+
+    question_no += 1 
+    question = input(f'\n(question_no). What does ROM stand for? ').lower()
+
+    if ques == 'read only memory':
+        print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> read only memory')
+
+    question_no += 1
+    question = input(f'\n(question_no). Is mango a fruit? ').lower()
+
+    if ques == 'Yes':
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Yes')
+
+    question_no += 1
+    question = input(f'\n(question_no).What city is the capital of Australia? ').lower()
+
+    if ques == 'Canberra': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Canberra')
+
+    question_no += 1
+    question = input(f'\n(question_no).What city is the capital of Japan? ').lower()
+
+    if ques == 'Tokyo': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Tokyo')
+
+    question_no += 1
+    question = input(f'\n(question_no).What is the value of 4 + 3? ').lower()
+
+    if ques == '7': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> 7')
+
+    question_no += 1
+    question = input(f'\n(question_no).What cis the value of 5 * 5? ').lower()
+
+    if ques == '25': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> 25')
+
+    question_no += 1
+    question = input(f'\n(question_no).What is the value of 9 + 2? ').lower()
+
+    if ques == '11': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> 11')
+
+    question_no += 1
+    question = input(f'\n(question_no).WWW stand for? ').lower()
+
+    if ques == 'World Wide Web': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> World Wide Web')
+
+    question_no += 1
+    question = input(f'\n(question_no).How many language are written from left to rught? ').lower()
+
+    if ques == '12': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> 12')
+
+    question_no += 1
+    question = input(f"s\n(question_no).What is the name of the world's largest ocean? ").lower()
+
+    if ques == 'Pacific Ocean': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Pacific Ocean')
+
+    question_no += 1
+    question = input(f'\n(question_no).What was the first drink in space? ').lower()
+
+    if ques == 'Coca Cola': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Coca Cola')
+
+    question_no += 1
+    question = input(f'\n(question_no).Which country invented Ice Cream? ').lower()
+
+    if ques == 'China': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> China')
+
+    question_no += 1
+    question = input(f'\n(question_no).Who won 2022 World Cup? ').lower()
+
+    if ques == 'Argentina': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Argentina')
+
+    question_no += 1
+    question = input(f"\n(question_no).What's the fastest land animal? ").lower()
+
+    if ques == 'Cheetah': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Cheetah')
+
+    question_no += 1
+    question = input(f'\n(question_no).What colur is Ruby? ').lower()
+
+    if ques == 'Red': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Red')
+
+    question_no += 1
+    question = input(f'\n(question_no).How many colours are ther in the Rainbow? ').lower()
+
+    if ques == 'Seven': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Seven')
+
+    question_no += 1
+    question = input(f'\n(question_no).What is Cynophobia? ').lower()
+
+    if ques == 'Fear of Dog': 
+       print('correct!')
+
+    else:
+        print('Incorrect!')
+        print(f'current answer is ---> Fear of Dog')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
